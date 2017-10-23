8d339b90b41144511069550fce8978b2a0356fba
xiegithub <541534530@qq.com>
2017-10-23 16:22:25 +0800

    arithmetic AES

---
+++ src/arithmetic/aes/AesTest.java
@@ -0,0 +1,53 @@
+package arithmetic.aes;
+
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+public class AesTest {
+	public static void main(String[] args) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+		String content = "1234567890123456";
+		/**
+		 * 创建AES的key生产者
+		 */
+		KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+		keyGenerator.init(128, new SecureRandom());
+		/**
+		 * 生成一个密钥
+		 */
+		SecretKey secretKey = keyGenerator.generateKey();
+		/**
+		 * 返回一个基本编码格式的一个密钥
+		 */
+		byte[] enCodeFormat = secretKey.getEncoded();
+		/**
+		 * 转换AES专用密钥
+		 */
+		SecretKey key = new SecretKeySpec(enCodeFormat, "AES");
+		/**
+		 * 创建加密器
+		 */
+		Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+		byte[] byteContent = content.getBytes();
+		/**
+		 * 初始化加密器
+		 */
+		cipher.init(Cipher.ENCRYPT_MODE, key);
+		/**
+		 * 加密数据
+		 */
+		byte[] result = cipher.doFinal(byteContent);
+		
+		String xxx = new String(result);
+		System.out.println(xxx);
+	}
+	
+}
